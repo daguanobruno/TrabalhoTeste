8587221fea13e4ff1f6f3b8f243f14b54e1eca3f
Bruno Daguano <bruno_daguano@hotmail.com>
2018-11-18 11:40:32 -0200

    Nono Caso de Teste Implementado

--- TrabalhoTeste/src/test/java/po/ItemsPage.java
+++ TrabalhoTeste/src/test/java/po/ItemsPage.java
@@ -18,13 +18,44 @@ public class ItemsPage extends AkauntingPage{
     @FindBy(xpath = "/html/body/div/div/section[1]/h1/span[1]/a\n")
     WebElement addItemsButton;
     
+    @FindBy(xpath = "//*[@id=\"tbl-items\"]/tbody/tr/td[8]/div/button")
+    WebElement clickButtom;
+    
+    @FindBy(xpath = "//*[@id=\"item-2\"]/button")
+    WebElement deleteItem;
+    
+    @FindBy(xpath = "//*[@id=\"confirm-modal\"]/div/div/div[3]/div/button[1]")
+    WebElement confirm;
+    
+    @FindBy(xpath = "/html/body/div/div/section[2]/div[1]")
+    WebElement mensagem;
+    
     public ItemsPage(WebDriver driver) {
         super(driver);
     }
     
-     public AddItems clickFindButton() {
+    public AddItems clickFindButton() {
         addItemsButton.click();
         return new AddItems(driver);
+    }
+    
+    public ItemsPage clickButton() {
+        clickButtom.click();
+        return new ItemsPage(driver);
+    }
+    
+    public ItemsPage clickDeleteButton() {
+        deleteItem.click();
+        return new ItemsPage(driver);
+    }
+    
+    public ItemsPage clickConfirmarExclusao() {
+        confirm.click();
+        return new ItemsPage(driver);
+    }
+    
+    public String setMensagemAviso() {
+        return mensagem.getText();
     }
     
 }
---
+++ TrabalhoTeste/src/test/java/trabalhoTeste/NonoCaseTest.java
@@ -0,0 +1,81 @@
+package trabalhoTeste;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import po.AddInvoices;
+import po.AddItems;
+import po.AddVendors;
+import po.AkauntingPage;
+import po.CategoriesPage;
+import po.CostumersPage;
+import po.HomePage;
+import po.InvoicesPage;
+import po.ItemsPage;
+import po.Login;
+import po.Menu;
+import po.ProfitLossPage;
+import po.TransactionPage;
+import po.VendorsPage;
+
+/**
+ *
+ * @author Usuario
+ */
+public class NonoCaseTest {
+
+    private WebDriver driver = new ChromeDriver();
+
+    @BeforeClass
+    public static void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @Before
+    public void before() {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("headless");
+        chromeOptions.addArguments("window-size=1200x600");
+        chromeOptions.addArguments("lang=en-US");
+        chromeOptions.addArguments("start-maximized");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void DeletarItem() {
+        HomePage homePage = new HomePage(driver);
+        Login login = new Login(driver);
+
+        login.setEmail("teste@teste.com").
+                setSenha("utfpr").
+                Logar();
+
+        ItemsPage Item = homePage.getMenu().goToIncomes().goToItems();
+
+        try {
+            Item.clickButton().clickDeleteButton();
+            Thread.sleep(3000);
+            Item.clickConfirmarExclusao();
+        } catch (Exception e) {
+            System.out.println("Deu erro!");
+        }
+        
+        String mensagem = Item.setMensagemAviso();
+        assertEquals("Warning: You are not allowed to delete Moto G5s Plus because it has 1 invoice related.",
+                mensagem);
+
+
+    }
+
+}
